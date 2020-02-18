0367c7394e653ab43f70493ff0ef569026dfd6a6
Jason Melton <jason.melton2@gmail.com>
2020-02-18 17:41:53 +0000

    Done.

--- lib/example.rb
+++ lib/example.rb
@@ -1,13 +1,17 @@
-# Use this space to code along with the readme
 
-phrase = "Hello World!"
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+
+def hello_ten_times
+  phrase = "Hello World!"
+  i = 0 
+  while i < 10 do 
+    puts phrase
+    i += 1 
+  end 
+end
+
+def diff_hello_ten_times
+  phrase = "Hello World!"
+  10.times {puts phrase}
+end 
+
+diff_hello_ten_times
